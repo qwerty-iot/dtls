1f7a13f4cc7bc4928b1565d68d7a4cf8993c5ec7
Jim Wert <jim.wert@telit.com>
2016-11-19 16:54:25 -0500

    Added ability to turn down logging.

--- common/log.go
+++ common/log.go
@@ -16,23 +16,51 @@ const (
 type LogFunc func(ts time.Time, level string, msg string)
 
 var logFunc LogFunc = defaultLogFunc
+var logLevel int = 3
+
+func LogLevelSet(level string) {
+	switch level {
+	case LogLevelError:
+		logLevel = 1
+	case LogLevelWarn:
+		logLevel = 2
+	case LogLevelInfo:
+		logLevel = 3
+	case LogLevelDebug:
+		logLevel = 4
+	default:
+		logLevel = 0
+	}
+}
 
 func defaultLogFunc(ts time.Time, level string, msg string) {
 	log.Printf(" [" + level + "] " + msg)
 }
 
 func LogError(f string, args ...interface{}) {
+	if logLevel < 1 {
+		return
+	}
 	logFunc(time.Now(), LogLevelError, fmt.Sprintf(f, args...))
 }
 
 func LogWarn(f string, args ...interface{}) {
+	if logLevel < 2 {
+		return
+	}
 	logFunc(time.Now(), LogLevelWarn, fmt.Sprintf(f, args...))
 }
 
 func LogInfo(f string, args ...interface{}) {
+	if logLevel < 3 {
+		return
+	}
 	logFunc(time.Now(), LogLevelInfo, fmt.Sprintf(f, args...))
 }
 
 func LogDebug(f string, args ...interface{}) {
+	if logLevel < 4 {
+		return
+	}
 	logFunc(time.Now(), LogLevelDebug, fmt.Sprintf(f, args...))
 }
